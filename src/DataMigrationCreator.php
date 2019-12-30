14dd1999804f8a1c0c75505f42dddbf23dd208c2
sabbir25112 <sabbir25112@gmail.com>
2019-12-30 11:52:25 +0600

    always use 'blank.stub' for creating data-migration

--- src/DataMigrationCreator.php
+++ src/DataMigrationCreator.php
@@ -4,13 +4,13 @@ use Illuminate\Database\Migrations\MigrationCreator;
 
 class DataMigrationCreator extends MigrationCreator
 {
-    /**
-     * Get the path to the Stubs.
-     *
-     * @return string
-     */
     public function stubPath()
     {
         return __DIR__.'/Stubs';
+    }
+
+    protected function getStub($table, $create)
+    {
+        return $this->files->get($this->stubPath().'/blank.stub');
     }
 }
